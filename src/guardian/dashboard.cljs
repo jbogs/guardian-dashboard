de1650946ec39737ae2e4de68f9b8afa0c6240c8
jumblerg <jumblerg@gmail.com>
2016-12-01 14:59:01 -0500

    stylize panels

--- src/guardian/dashboard.cljs
+++ src/guardian/dashboard.cljs
@@ -85,81 +85,83 @@
 ;-- sizes ---------------------------------------------------------------------;
 
 (def l 2)
-(def g 20)
+(def g-sm 6)
+(def g-lg 20)
 
 ;-- colors --------------------------------------------------------------------;
 
 (def white (c 0xffffff))
-(def grey  (c 0x4d4d4d))
+(def bgrey (c 0x4d4d4d))
+(def cgrey (c 0x161616))
 (def black (c 0x111111))
 (def red   (c 0xc32026))
 
 ;-- fonts ---------------------------------------------------------------------;
 
-(def body-font   {:f 20 :ff ["Helvetica Neue" "Lucida Grande" :sans-serif] :fc grey})
-(def button-font {:f 14 :ff ["Helvetica Neue" "Lucida Grande" :sans-serif] :fc grey})
+(def body-font   {:f 20 :ff ["Helvetica Neue" "Lucida Grande" :sans-serif] :fc bgrey})
+(def button-font {:f 14 :ff ["Helvetica Neue" "Lucida Grande" :sans-serif] :fc bgrey})
 (def title-font button-font)
 
 ;;; views ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defelem panel [{:keys [icon name] :as attrs} elems]
-  (elem :gv 1 (dissoc attrs :icon :name)
-    (elem :sh (r 1 1) :c black :p g :gh g
-      (image :sv 40 :a :mid :url icon)
-      (elem :sv 40 :a :mid :fc grey
+  (elem (dissoc attrs :icon :name) :fc bgrey
+    (elem :sh (r 1 1) :c black :p g-lg :gh g-lg
+      (image :sv 40 :av :mid :url icon)
+      (elem :sv 40 :av :mid
         name))
-    (elem :sh (r 1 1) :sv (- (r 1 1) 40 g) :gv l
+    (elem :sh (r 1 1) :sv (- (r 1 1) 40 g-lg) :pv g-sm :ph g-lg :c cgrey
       elems)))
 
 (defelem panel-table [{:keys [name] :as attrs} elems]
-  (elem :g l (dissoc attrs :name)
-    (elem :sh (r 1 1) :p g :c black :fc grey
+  (elem (dissoc attrs :name)
+    (elem :sh (r 1 1) :p g-sm :ah :mid
       name)
     elems))
 
 (defelem panel-row [{:keys [name] :as attrs} elems]
-  (elem :gh l :fc grey (dissoc attrs :name)
-    (elem :sh (r 1 2) :p g :c black
+  (elem :fc bgrey :pv g-sm (dissoc attrs :name)
+    (elem :sh (r 1 2)
       name)
-    (elem :sh (r 1 2) :p g :c black 
+    (elem :sh (r 1 2) :ah :end
       elems)))
 
 (defn lighting-view []
-  (elem title-font :sh (>sm (- (r 1 1) 60 2)) :sv (r 1 1) :p g :g g :c black
+  (elem title-font :sh (>sm (- (r 1 1) 60 2)) :sv (r 1 1) :p g-lg :g g-lg :c cgrey
     (elem "LIGHTING VIEW")))
 
 (defn fans-view []
-  (elem title-font :sh (>sm (- (r 1 1) 60 2)) :sv (r 1 1) :p g :g g :c black
+  (elem title-font :sh (>sm (- (r 1 1) 60 2)) :sv (r 1 1) :p g-lg :g g-lg :c cgrey
     (elem "FANS VIEW")))
 
 (defn health-view []
-  (elem title-font :sh (>sm (- (r 1 1) 60 2)) :sv (r 1 1) :p g :g g :c black
+  (elem title-font :sh (>sm (- (r 1 1) 60 2)) :sv (r 1 1) :p g-lg :g g-lg :c cgrey
     (elem "HEALTH VIEW")))
 
 (defn info-view []
-  (elem title-font :sh (>sm (- (r 1 1) 60 2)) :sv (r 1 1) :p g :g g :c black
-    (elem :sh (r 1 2) :p g :g g :c black :av :mid
+  (elem title-font :sh (>sm (- (r 1 1) 60 2)) :sv (r 1 1) :p g-lg :g g-lg :c cgrey
+    (elem :sh (r 1 2) :p g-lg :g g-lg :c black :av :mid
       (image :url "pc-icon.svg")
       "PC NAME")
-    (elem :sh (r 1 2) :p g :g g :c black :av :mid
+    (elem :sh (r 1 2) :p g-lg :g g-lg :c black :av :mid
       (image :url "os-icon.svg")
       "OPERATING SYSTEM")
-    (elem :sh (r 1 2) :p g :g g :c black :av :beg
+    (elem :sh (r 1 2) :p g-lg :g g-lg :c black :av :beg
       (image :s 40 :a :mid :url "processor-icon.svg")
       (elem :sv 40 :a :mid "PROCESSOR"))
-    (elem :sh (r 1 2) :p g :g g :c black :av :beg
+    (elem :sh (r 1 2) :p g-lg :g g-lg :c black :av :beg
       (image :s 40 :a :mid :url "video-card-icon.svg")
       (elem :sv 40 :a :mid"VIDEO CARD"))
-    (elem :sh (r 1 2) :p g :g g :c black :av :beg
+    (elem :sh (r 1 2) :p g-lg :g g-lg :c black :av :beg
       (image :s 40 :a :mid :url "motherboard-icon.svg")
       (elem :sv 40 :a :mid "MOTHERBOARD"))
-    (elem :sh (r 1 2) :p g :g g :c black :av :beg
+    (elem :sh (r 1 2) :p g-lg :g g-lg :c black :av :beg
       (image :s 40 :a :mid :url "memory-icon.svg")
       (elem :sv 40 :a :mid "MEMORY"))
-    (elem :sh (r 1 2) :p g :g g :c black :av :beg
+    (elem :sh (r 1 2) :p g-lg :g g-lg :c black :av :beg
       (image :s 40 :a :mid :url "drive-icon.svg")
       (elem :sv 40 :a :mid "HDD 1"))
-    (elem :sh (r 1 2) :p g :g g :c black :av :beg
+    (elem :sh (r 1 2) :p g-lg :g g-lg :c black :av :beg
       (image :s 40 :a :mid :url "drive-icon.svg")
       (elem :sv 40 :a :mid "HDD 2"))))
 
@@ -168,26 +170,26 @@
   :route        (cell= [[view]])
   :initiated    initiate!
   :routechanged change-route!
-  :scroll true :c grey :g l
+  :scroll true :c bgrey :g l
   (elem :sh (r 1 1) :ah :mid :c black
-    (elem :sh (r 1 1) :ah (b :mid sm :beg) :av (b :beg sm :mid) :p g :gv g
+    (elem :sh (r 1 1) :ah (b :mid sm :beg) :av (b :beg sm :mid) :p g-lg :gv g-lg
       (image :sh 200 :url "xotic-pc-logo.svg" :m :pointer :click #(.open js/window "https://www.xoticpc.com"))
-      (elem :sh (>sm (- (r 1 1) 200)) :ah (b :mid sm :end) :gh (* 2 g)
+      (elem :sh (>sm (- (r 1 1) 200)) :ah (b :mid sm :end) :gh (* 2 g-lg)
         (for [[logo link] footer-menu-items]
           (image :m :pointer :url logo :click #(.open js/window link))))))
   (elem :sh (r 1 1) :sv (- (r 1 1) 86 300 (* l 2)) :gh l
     (elem :sh (>sm 60) :ah :mid :gv l
-      (elem :s 60 :a :mid :fc grey :c black :m :pointer :click #(change-view! :health)   "S")
-      (elem :s 60 :a :mid :fc grey :c black :m :pointer :click #(change-view! :lighting) "L")
-      (elem :s 60 :a :mid :fc grey :c black :m :pointer :click #(change-view! :fans)     "F")
-      (elem :s 60 :a :mid :fc grey :c black :m :pointer :click #(change-view! :info)     "I")
+      (elem :s 60 :a :mid :fc bgrey :c black :m :pointer :click #(change-view! :health)   "S")
+      (elem :s 60 :a :mid :fc bgrey :c black :m :pointer :click #(change-view! :lighting) "L")
+      (elem :s 60 :a :mid :fc bgrey :c black :m :pointer :click #(change-view! :fans)     "F")
+      (elem :s 60 :a :mid :fc bgrey :c black :m :pointer :click #(change-view! :info)     "I")
       (elem :sh 60 :sv (- (r 1 1) (* 60 4)) :c black))
     (case-tpl view
       :health   (health-view)
       :lighting (lighting-view)
       :fans     (fans-view)
       :info     (info-view)))
-  (elem :sh (r 1 1) :sv 300 :gh l
+  (elem :sh (r 1 1) :sv 544 :gh l
     (panel :sh (r 1 4) :sv (r 1 1) :name "MBS" :icon "motherboard-icon.svg"
       (for-tpl [{:keys [name temps fans]} (cell= (:mbs data))]
         (panel-table :sh (r 1 1) :name name
