d30e358e0c525bd668db584fbc17610c0af7c3c8
jumblerg <jumblerg@gmail.com>
2016-12-14 02:56:35 -0500

    transform hard drive data

--- src/guardian/dashboard/transformations.cljs
+++ src/guardian/dashboard/transformations.cljs
@@ -11,12 +11,19 @@
                  :load  (some #(when (= "UC"      (:name %)) %) loads)
                  :cores (mapv #(assoc % :threads %2) cores (partition 2 threads)))))))
 
+(defn xform-hdd [{:keys [name loads temps] :as hdd}]
+  (-> (dissoc hdd :loads :temps)
+      (assoc :volume (-> loads first :name)) ;; can't even use textual name here, will break relying on index
+             :used   (-> loads first :value))
+             :temp   (some #(when (= "Assembly" (:name %)) %) temps))
+
 (defn monitor-data [data]
   (let [init     #(subs % 0 (dec (count %)))
         type-seq (comp keyword init name)
         into-seq #(into % (mapv (fn [x] (assoc x :type (type-seq %2))) %3))
         conj-map #(conj % (assoc %3 :type %2))]
   (->> (update data :cpus (partial mapv xform-cpu))
+       (update data :hdds (partial mapv xform-hdd))
        (reduce-kv #(if (sequential? %3) (into-seq %1 %2 %3) (conj-map %1 %2 %3)) [])
        (assoc {} :components))))
 
