12611acf1baae8cf16fb1f831fb7bfee767f3727
jumblerg <jumblerg@gmail.com>
2017-01-25 01:24:52 -0500

    wip

--- src/guardian/dashboard/transformations.cljs
+++ src/guardian/dashboard/transformations.cljs
@@ -3,6 +3,8 @@
     [clojure.set :refer [rename-keys]]
     [cljsjs.d3]))
 
+(def ^:dynamic *time* nil)
+
 ;;; utils ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn hsl->rgb [[h s l]]
@@ -14,6 +16,11 @@
     (mapv int [(.-h c) (.-s c) (.-l c)])))
 
 ;;; xforms ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn msi-config
+  {"THRM" {:id "THRM" :name "Thermal Zone"}
+   "TZ00" {:id "TZ00" :name "Thermal Zone 0"}
+   "TZ01" {:id "TZ01" :name "Thermal Zone 1"}})
 
 (defn xform-cpu [{:keys [name temps loads volts] :as cpu}]
   (when cpu
@@ -36,26 +43,42 @@
                    (dissoc :end_color)
                    (update :color rgb->hsl)
                    (assoc :name (str "Zone " (:zone %))))]
-    {:name  "Keyboard"
+    {:name  "Keyboardz"
      :zones (->> (dissoc keyboard :all) (sort first) (mapv (comp xform second)))}))
+
+{:name "Micro-Star International Co., Ltd. MS-16H8"
+ :fans  []
+ :north-bridge [{:time, :value 44} {:name "TZ00", :value 27.8} {:name "TZ01", :value 29.8}]
+ :volts []}
+
+{:name "Micro-Star International Co., Ltd. MS-16H8"
+ :fans []
+ :temps [{:name "THRM", :value 44} {:name "TZ00", :value 27.8} {:name "TZ01", :value 29.8}]
+ :volts []}
+
+(defn xform-mb [data]
+  (prn :data data :time *time*)
+  data)
 
 (defn xform-memory [{:keys [name free total] :as memory}] ;; no name available
   (-> (dissoc memory :total)
       (assoc :used (- total free) :name "Memory")))
 
 (defn sensor-data [data]
-  (let [init     #(subs % 0 (dec (count %)))
-        type-seq (comp keyword init name)
-        into-seq #(into % (mapv (fn [x] (assoc x :type (type-seq %2))) %3))
-        conj-map #(conj % (assoc %3 :type %2))]
-  (as-> data $
-        (rename-keys $ {:led_keyboard :keyboard})
-        (update $ :cpus     (partial mapv xform-cpu))
-        (update $ :hdds     (partial mapv xform-hdd))
-        (update $ :memory   xform-memory)
-        (update $ :keyboard xform-keyboard)
-        (reduce-kv #(if (sequential? %3) (into-seq %1 %2 %3) (conj-map %1 %2 %3)) [] $)
-        (assoc {} :components $))))
+  (binding [*time* (.now js/Date)]
+    (let [init     #(subs % 0 (dec (count %)))
+          type-seq (comp keyword init name)
+          into-seq #(into % (mapv (fn [x] (assoc x :type (type-seq %2))) %3))
+          conj-map #(conj % (assoc %3 :type %2))]
+      (as-> data $
+            (rename-keys $ {:led_keyboard :keyboard})
+            (update $ :mb       xform-mb)
+            (update $ :cpus     (partial mapv xform-cpu))
+            (update $ :hdds     (partial mapv xform-hdd))
+            (update $ :memory   xform-memory)
+            (update $ :keyboard xform-keyboard)
+            (reduce-kv #(if (sequential? %3) (into-seq %1 %2 %3) (conj-map %1 %2 %3)) [] $)
+            (assoc {} :components $)))))
 
 (defn xform [data]
   (cond
