c6fde5d45b213dd7d62af19e6148d398cf94ecba
jumblerg <jumblerg@gmail.com>
2017-02-10 18:59:58 -0500

    make data model less dynamic

--- src/guardian/dashboard/transformations.cljs
+++ src/guardian/dashboard/transformations.cljs
@@ -16,70 +16,89 @@
 ;;; config ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (def sensors
-  {:cpu-temp    "Package"
-   :cpu-load    "UC"
-   :hdd-temp    "Assembly"
-   :zone-1-temp "THRM"
-   :zone-2-temp "TZ00"
-   :zone-3-temp "TZ01"})
+  {:cpu-power         "Package"
+   :cpu-temp          "Package"
+   :cpu-load          "UC"
+   :gpu-processor     "GPU"
+   :gpu-memory        "Memory"
+   :gpu-frame-buffer  "Frame Buffer"
+   :gpu-video-engine  "Video Engine"
+   :gpu-bus-interface "Bus Interface"
+   :hdd-temp          "Assembly"
+   :zone-1            "THRM"
+   :zone-2            "TZ00"
+   :zone-3            "TZ01"})
 
-(def computer
-  {:name    "MSI Gaming Series G Laptop"
-   :sensors sensors
-   :zones   [{:name "CPU"}
-             {:name "North Bridge"}
-             {:name "South Bridge"}]})
+(defn name->sensor [reading]
+  (rename-keys reading {:name :sensor}))
+
+(defn get-sensor [coll sensor]
+ (-> (some #(when (= (sensor sensors) (:name %)) %) coll)
+     (name->sensor)))
 
 ;;; xforms ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defn xform-mb [mb]
-  (-> (dissoc mb :fans :temps :volts)
-      (assoc :zones (mapv #(hash-map :name (:name %1) :temp %2) (:zones computer) (:temps mb)))))
+(defn cpu [{:keys [name clocks loads temps volts watts]}]
+  (let [temps* (remove #(= (:name %) (:cpu-temp sensors)) temps)
+        loads* (remove #(= (:name %) (:cpu-load sensors)) loads)
+        cores* (mapv #(hash-map :name (:name %) :freq (name->sensor %1) :temp (name->sensor %2)) clocks temps*)]
+    {:name  name
+     :temp  (get-sensor temps :cpu-temp)
+     :load  (get-sensor loads :cpu-load)
+     :cores (mapv #(assoc % :threads %2) cores* (partition 2 loads*))}))
 
-(defn xform-cpu [{:keys [name temps loads volts] :as cpu}]
-  (when cpu
-    (let [cores   (mapv (fn [t] (rename-keys t {:value :temp})) (remove #(= (:name %) (:cpu-temp sensors)) temps))
-          threads (mapv (fn [l] (rename-keys l {:value :load})) (remove #(= (:name %) (:cpu-load sensors)) loads))]
-      (-> (dissoc cpu :temps :loads)
-          (assoc :temp  (some #(when (= (:cpu-temp sensors) (:name %)) %) temps)
-                 :load  (some #(when (= (:cpu-load sensors) (:name %)) %) loads)
-                 :cores (mapv #(assoc % :threads %2) cores (partition 2 threads)))))))
+(defn drive [{:keys [name loads temps]}]
+  {:name   name
+   :volume (-> loads first :name)
+   :used   (-> loads first name->sensor)
+   :free   (-> loads first name->sensor (update :value (partial - 100)))
+   :temp   (get-sensor temps :hdd-temp)})
 
-(defn xform-hdd [{:keys [name loads temps] :as hdd}]
-  (-> (dissoc hdd :loads :temps)
-      (assoc :volume (-> loads first :name) ;; can't even use textual name here, will break relying on index
-             :used   (-> loads first :value)
-             :free   (- 100 (-> loads first :value))
-             :temp   (some #(when (= (:hdd-temp sensors) (:name %)) %) temps))))
+(defn card [{:keys [name loads temps]}]
+  {:name          name
+   :type          :gpu
+   :gpu           {:name "GPU"
+                   :temp (get-sensor loads :gpu-processor)
+                   :load (get-sensor temps :gpu-processor)}
+   :memory        {:name "Memory"
+                   :load (get-sensor loads :gpu-memory)}
+   :frame-buffer  {:name "Frame Buffer"
+                   :load (get-sensor loads :gpu-frame-buffer)}
+   :video-engine  {:name "Video Engine"
+                   :temp (get-sensor loads :gpu-video-engine)}
+   :bus-interface {:name "Bus Interface"
+                   :temp (get-sensor loads :gpu-bus-interface)}})
 
-(defn xform-keyboard [keyboard]
-  (let [xform #(-> (rename-keys % {:zone :id}) ;; todo: start, stop colors
-                   (dissoc :end_color)
-                   (update :color rgb->hsl)
-                   (assoc :name (str "Zone " (:zone %))))]
+(defn keyboard [keyboard]
+  (let [zone #(hash-map
+                 :name  (->> % :zone (str "Zone "))
+                 :color (->> % :color rgb->hsl))
+        zones (->> (dissoc keyboard :all)
+                   (sort first)
+                   (mapv (comp zone second)))]
     {:name  "Keyboard"
-     :zones (->> (dissoc keyboard :all) (sort first) (mapv (comp xform second)))}))
+     :zones zones}))
 
-(defn xform-memory [{:keys [name free total] :as memory}] ;; no name available
-  (-> (dissoc memory :total)
-      (assoc :used (- total free) :name "Memory")))
+(defn memory [{:keys [name free total] :as memory}]
+  {:name "Memory"
+   :used {:value (- total free)}
+   :free {:value free}})
 
-(defn sensor-data [data]
-  (let [init     #(subs % 0 (dec (count %)))
-        type-seq (comp keyword init name)
-        into-seq #(into % (mapv (fn [x] (assoc x :type (type-seq %2))) %3))
-        conj-map #(conj % (assoc %3 :type %2))]
-  (as-> data $
-        (rename-keys $ {:led_keyboard :keyboard})
-        (update $ :mb       xform-mb)
-        (update $ :cpus     (partial mapv xform-cpu))
-        (update $ :hdds     (partial mapv xform-hdd))
-        (update $ :memory   xform-memory)
-        (update $ :keyboard xform-keyboard)
-        (reduce-kv #(if (sequential? %3) (into-seq %1 %2 %3) (conj-map %1 %2 %3)) [] $)
-        (assoc {} :components $))))
+(defn motherboard [{{:keys [name temps]} :mb mem :memory kb :led_keyboard :keys [cpus gpus hdds]}]
+  {:name   name
+   :zone-1   {:name "CPU Thermal Zone"
+              :desc "Located next to the CPU core on Intel boards"
+              :temp (get-sensor temps :zone-1)}
+   :zone-2   {:name "North Bridge Thermal Zone"
+              :desc "Located next to the CPU socket on Intel boards"
+              :temp (get-sensor temps :zone-2)}
+   :zone-3   {:name "South Bridge Thermal Zone"
+              :desc "Located next to the memory slots on Intel boards"
+              :temp (get-sensor temps :zone-3)}
+   :cpu       (cpu     (first cpus))
+   :gpu       {:name (-> gpus first :name)}
+   :memory   (memory mem)
+   :keyboard (keyboard kb)
+   :cards    (mapv card  (rest gpus))
+   :drives   (mapv drive       hdds)})
 
-(defn xform [data]
-  (cond
-    (:mb data) (sensor-data data)
-    :else      data))
